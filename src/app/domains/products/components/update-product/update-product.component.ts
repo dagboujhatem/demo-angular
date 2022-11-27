1c2f785d19fa8adb32ab85d687b0cd9d3584197e
dagbouj hatem <dagboujhatem@gmail.com>
2022-11-27 09:10:22 +0100

    update product

--- src/app/domains/products/components/update-product/update-product.component.ts
+++ src/app/domains/products/components/update-product/update-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update-product',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateProductComponent implements OnInit {
 
+  productForm?: FormGroup;
+  submitted = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.productForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      price: new FormControl('', [Validators.required]),
+      stock: new FormArray([
+        new FormGroup({
+          quantity: new FormControl('', [Validators.required]),
+          size: new FormControl('', [Validators.required]),
+          
+        })
+      ]),
+    });
+  }
+
+  get stock(): FormArray {
+    return this.productForm?.get('stock') as FormArray;
+  }
+
+  addNew(): void {
+    this.stock.push(new FormGroup({
+      quantity: new FormControl('', [Validators.required]),
+      size: new FormControl('', [Validators.required]),
+    }))
+  }
+
+  clearAll(): void {
+    this.stock.clear();
+  }
+
+  deleteControl(i: number): void {
+    this.stock.removeAt(i);
+  }
+
+  createProduct(): void {
+    this.submitted = true;
+    if (this.productForm?.invalid) {
+      return;
+    }
+
+    console.log(this.productForm?.value)
+    // Call to RESTfull API
+
+
   }
 
 }
