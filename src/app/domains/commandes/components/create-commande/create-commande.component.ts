0571b88c4dd0846d55cfebb1ffd30acdab16aba6
dagbouj hatem <dagboujhatem@gmail.com>
2022-11-27 11:17:01 +0100

    formulaires

--- src/app/domains/commandes/components/create-commande/create-commande.component.ts
+++ src/app/domains/commandes/components/create-commande/create-commande.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-commande',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateCommandeComponent implements OnInit {
 
+  commandeForm?: FormGroup;
+  submitted = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.commandeForm = new FormGroup({
+      total: new FormControl('', [Validators.required]),
+    });
+    this.formChangeEvent();
+  }
+
+  formChangeEvent(): void{
+    this.commandeForm?.get('total')?.valueChanges.subscribe(value=>{
+      // console.log(value);
+      if(value>100){
+        this.commandeForm?.addControl('card', new FormControl('', [Validators.required]));
+        // modifier les validators d'un autre champs
+        this.commandeForm?.get('total')?.clearValidators();
+        this.commandeForm?.get('total')?.setValidators([Validators.required, Validators.max(1000)]);
+      }else{
+        this.commandeForm?.removeControl('card');
+      }
+    });
+  }
+
+  createCommande(): void {
+    this.submitted = true;
+    if(this.commandeForm?.invalid){
+      return;
+    }
+    // print in console
+    console.log(this.commandeForm?.value)
+    // call REST API
   }
 
 }
