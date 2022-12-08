03608bbd558654957b9ea92ac843c0aad0ae4527
dagbouj hatem <dagboujhatem@gmail.com>
2022-12-08 20:51:21 +0100

    delete spec files

--- src/app/domains/posts/pipes/search2.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Search2Pipe } from './search2.pipe';
-
-describe('Search2Pipe', () => {
-  it('create an instance', () => {
-    const pipe = new Search2Pipe();
-    expect(pipe).toBeTruthy();
-  });
-});
